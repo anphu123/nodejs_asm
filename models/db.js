39d88f0bd67832efabaa5d853d04a7107d364cff
anphu123 <anphu221002@gmail.com>
2024-04-09 04:55:39 +0700

    a

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/b')
+mongoose.connect('mongodb+srv://anphu221002:anphu123@cluster0.bjvupgu.mongodb.net/test1')
 .catch((err)=>{
     console.log("loi ket noi csdl");
     console.log(err);
